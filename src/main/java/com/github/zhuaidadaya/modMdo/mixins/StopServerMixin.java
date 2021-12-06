83befa49fe098b62d3953e4950041fed466667f8
Zhuaidadaya <1501917367@qq.com>
2021-12-06 15:54:54 +0800

    from branch 1.18

---
+++ src/main/java/com/github/zhuaidadaya/modMdo/mixins/StopServerMixin.java
@@ -0,0 +1,43 @@
+package com.github.zhuaidadaya.modMdo.mixins;
+
+
+import net.minecraft.server.rcon.RconBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static com.github.zhuaidadaya.modMdo.Storage.Variables.LOGGER;
+import static com.github.zhuaidadaya.modMdo.Storage.Variables.server;
+
+@Mixin(RconBase.class)
+public class StopServerMixin {
+
+    /**
+     * @author
+     */
+    @Inject(at = @At("HEAD"),method = "stop")
+    public void stop(CallbackInfo ci) {
+        if(server.isRunning()) {
+            if(server.getThread() != null) {
+                int i = 0;
+
+                while(server.getThread().isAlive()) {
+                    try {
+                        LOGGER.info("failed to stop server, ModMdo trying stop again");
+
+                        server.stop(false);
+                        i++;
+
+                        if(i > 5) {
+                            Runtime.getRuntime().exit(0);
+                            LOGGER.info("failed to stop server in 5 times try, ModMdo trying force stop task");
+                        }
+                    } catch(Exception ex) {
+
+                    }
+                }
+            }
+        }
+    }
+}
