7bdf22f60acf7c2b2ccc1d6480aec7013866308f
Zhuaidadaya <1501917367@qq.com>
2022-04-07 23:44:02 +0800

    .

--- src/main/java/com/github/zhuaidadaya/modMdo/utils/translate/TranslateUtil.java
+++
@@ -1,9 +0,0 @@
-package com.github.zhuaidadaya.modmdo.utils.translate;
-
-import net.minecraft.text.TranslatableText;
-
-public class TranslateUtil {
-    public static TranslatableText formatRule(String head, String info) {
-        return new TranslatableText(head + "." + info + ".rule.format");
-    }
-}
