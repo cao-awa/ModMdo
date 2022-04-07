6dfe692ce371c813d4ebe78aa9af2c6d5838b999
Zhuaidadaya <1501917367@qq.com>
2022-04-08 02:08:47 +0800

    ...

--- src/main/java/com/github/zhuaidadaya/modmdo/format/console/ConsoleTextFormat.java
+++ src/main/java/com/github/zhuaidadaya/modmdo/format/console/ConsoleTextFormat.java
@@ -1,40 +1,22 @@
 package com.github.zhuaidadaya.modmdo.format.console;
 
-import com.github.zhuaidadaya.modmdo.lang.Language;
-import com.github.zhuaidadaya.modmdo.reads.FileReads;
-import com.github.zhuaidadaya.modmdo.resourceLoader.Resources;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import com.github.zhuaidadaya.modmdo.format.LanguageResource;
+import com.github.zhuaidadaya.modmdo.format.TextFormat;
 
 import static com.github.zhuaidadaya.modmdo.storage.Variables.language;
 
-public class ConsoleTextFormat {
-    private Map<Language, JSONObject> format = new HashMap<>();
-
+public class ConsoleTextFormat extends TextFormat<String> {
     public ConsoleTextFormat(LanguageResource languageResource) {
-        for(Language lang : languageResource.getNames()) {
-            String resource = languageResource.get(lang);
-
-            try {
-                JSONObject json = new JSONObject(FileReads.read(new BufferedReader(new InputStreamReader(Resources.getResource(resource, getClass())))));
-                format.put(lang,json);
-            } catch (Exception e) {
-
-            }
-        }
+        super(languageResource);
     }
 
-    public String format(String source,Object... args) {
+    public String format(String source, Object... args) {
         try {
             String formatReturn = format.get(language).getString(source);
 
             for(Object o : args) {
                 try {
-                    formatReturn = formatReturn.formatted(o.toString());
+                    formatReturn = formatReturn.replaceFirst("%s" ,o.toString());
                 } catch (Exception ex) {
                     return formatReturn;
                 }
