d1c2930a04c48da667fe3d80654a202f4a5a8b29
cao-awa <3313220756@qq.com>
2022-08-23 03:38:36 +0800

    Merge branch 'main' of https://github.com/zhuaidadaya/modMdo into 1.18.x

--- src/main/java/com/github/cao/awa/modmdo/mixins/player/advancement/tracker/PlayerAdvancementTrackerMixin.java
+++ src/main/java/com/github/cao/awa/modmdo/mixins/player/advancement/tracker/PlayerAdvancementTrackerMixin.java
@@ -1,23 +1,9 @@
 package com.github.cao.awa.modmdo.mixins.player.advancement.tracker;
 
-import com.github.cao.awa.modmdo.utils.times.*;
 import net.minecraft.advancement.*;
-import net.minecraft.server.*;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.*;
 
 @Mixin(PlayerAdvancementTracker.class)
 public class PlayerAdvancementTrackerMixin {
-    private long time = 0;
 
-    @Inject(method = "load", at = @At("HEAD"))
-    public void startLoad(ServerAdvancementLoader advancementLoader, CallbackInfo ci) {
-        time = TimeUtil.millions();
-    }
-
-    @Inject(method = "load", at = @At("RETURN"))
-    public void doneLoad(ServerAdvancementLoader advancementLoader, CallbackInfo ci) {
-        System.out.println("load done in " + TimeUtil.processMillion(time) + "ms");
-    }
 }
